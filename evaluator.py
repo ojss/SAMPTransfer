b504993c718cadc67c32b8be129df62f584cad86
Ojas Shirekar <ojas.shirekar@ojas.dev>
2022-04-03 09:08:45 +0200

    updated evaluator

--- evaluator.py
+++ evaluator.py
@@ -1,33 +1,31 @@
-import torch
 import argparse
 
-from bow.feature_extractor import CNN_4Layer
-from protoclr_obow import PCLROBoW
-from dataloaders import UnlabelledDataModule
 import pytorch_lightning as pl
+import torch
+
+from bow.feature_extractor import CNN_4Layer
 from callbacks import ConfidenceIntervalCallback
+from dataloaders import UnlabelledDataModule
+from protoclr_obow import PCLROBoW
 
 
 def cli_main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--c", type=str)
     args = parser.parse_args()
-    model=PCLROBoW(n_support=1,n_query=3,batch_size=64,lr_decay_step=25000,lr_decay_rate=0.5,feature_extractor=CNN_4Layer(3,64,64),bow_levels=['block4'],bow_extractor_opts={'inv_delta':15,'num_words':8192},bow_predictor_opts={'kappa':5})
-    xs = torch.load("ckpts/model_net_checkpoint_335.pth.tar", map_location='cpu')
-    model.load_state_dict(xs['network'])
-#    model = PCLROBoW.load_from_checkpoint(args.c,
-#                                          map_location=torch.device('cpu'), n_support=1, n_query=3, batch_size=64,
-#                                          lr_decay_step=25000, lr_decay_rate=0.5,
-#                                          feature_extractor=CNN_4Layer(3, 64, 64), bow_levels=['block4'],
-#                                          bow_extractor_opts={'inv_delta': 15, 'num_words': 4096},
-#                                          bow_predictor_opts={'kappa': 5})
+    model = PCLROBoW(n_support=1, n_query=3, batch_size=64, lr_decay_step=25000, lr_decay_rate=0.5,
+                     feature_extractor=CNN_4Layer(3, 64, 64), bow_levels=['block4'],
+                     bow_extractor_opts={'inv_delta': 15, 'num_words': 8192}, bow_predictor_opts={'kappa': 5})
+    # xs = torch.load("ckpts/model_net_checkpoint_335.pth.tar", map_location='cpu')
+    # model.load_state_dict(xs['network'])
+    model.load_from_checkpoint(args.c)
     datamodule = UnlabelledDataModule(dataset='miniimagenet',
                                       datapath='/home/nfs/oshirekar/unsupervised_ml/data/',
                                       split='test',
                                       img_size_orig=(84, 84),
                                       img_size_crop=(60, 60))
     trainer = pl.Trainer(
-        # gpus=-1,
+        gpus=-1,
         limit_test_batches=600,
         callbacks=[ConfidenceIntervalCallback(log_to_wb=False)]
     )
