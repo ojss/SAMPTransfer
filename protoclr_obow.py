81a570270e531601435a99b055cf60f3a1b28923
Ojas Shirekar <ojas.shirekar@ojas.dev>
2022-04-06 16:18:54 +0200

    created a submitit runner

--- protoclr_obow.py
+++ protoclr_obow.py
@@ -741,27 +741,28 @@ class MyCLI(LightningCLI):
 
         parser.add_argument("bow_predictor_opts.kappa", default=8)
 
+        parser.add_argument("job_name", default="local_dev_run", type=str, help="Job name")
         parser.add_argument(
-            "--nodes", default=4, type=int, help="Number of nodes to request"
+            "slurm.nodes", default=1, type=int, help="Number of nodes to request"
+        )
+        # parser.add_argument(
+        #     "slurm.ngpus", default=1, type=int, help="Number of gpus to request on each node"
+        # )
+        parser.add_argument(
+            "slurm.timeout", default=72, type=int, help="Duration of the job, in hours"
         )
         parser.add_argument(
-            "--ngpus", default=8, type=int, help="Number of gpus to request on each node"
+            "slurm.partition", default="general", type=str, help="Partition where to submit"
         )
+        parser.add_argument("slurm.slurm_additional_parameters", type=dict)
         parser.add_argument(
-            "--timeout", default=72, type=int, help="Duration of the job, in hours"
-        )
-        parser.add_argument("--job_name", default="vicreg", type=str, help="Job name")
-        parser.add_argument(
-            "--partition", default="mypartition", type=str, help="Partition where to submit"
-        )
-        parser.add_argument(
-            "--constraint",
+            "slurm.constraint",
             default="",
             type=str,
             help="Slurm constraint. Use 'volta32gb' for Tesla V100 with 32GB",
         )
         parser.add_argument(
-            "--comment",
+            "slurm.comment",
             default="",
             type=str,
             help="Comment to pass to scheduler, e.g. priority message")
@@ -770,6 +771,11 @@ class MyCLI(LightningCLI):
 def cli_main():
     cli = MyCLI(PCLROBoW, UnlabelledDataModule, run=False, save_config_overwrite=True,
                 parser_kwargs={"parser_mode": "omegaconf"})
+    cli.trainer.fit(cli.model, cli.datamodule)
+    cli.trainer.test(datamodule=cli.datamodule)
+
+
+def slurm_main(cli):
     cli.trainer.fit(cli.model, cli.datamodule)
     cli.trainer.test(datamodule=cli.datamodule)
 
--- run_with_submitit.py
+++ run_with_submitit.py
@@ -12,62 +12,42 @@ import uuid
 from pathlib import Path
 
 import protoclr_obow
+from dataloaders import UnlabelledDataModule
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        "Submitit for VICReg", parents=[protoclr_obow.MyCLI().]
-    )
-    parser.add_argument(
-        "--nodes", default=4, type=int, help="Number of nodes to request"
-    )
-    parser.add_argument(
-        "--ngpus", default=8, type=int, help="Number of gpus to request on each node"
-    )
-    parser.add_argument(
-        "--timeout", default=72, type=int, help="Duration of the job, in hours"
-    )
-    parser.add_argument("--job_name", default="vicreg", type=str, help="Job name")
-    parser.add_argument(
-        "--partition", default="mypartition", type=str, help="Partition where to submit"
-    )
-    parser.add_argument(
-        "--constraint",
-        default="",
-        type=str,
-        help="Slurm constraint. Use 'volta32gb' for Tesla V100 with 32GB",
-    )
-    parser.add_argument(
-        "--comment",
-        default="",
-        type=str,
-        help="Comment to pass to scheduler, e.g. priority message",
-    )
-    return parser.parse_args()
+    cli = protoclr_obow.MyCLI(protoclr_obow.PCLROBoW, UnlabelledDataModule,
+                              run=False,
+                              save_config_overwrite=True,
+                              parser_kwargs={"parser_mode": "omegaconf"})
+    return cli
 
 
 def main():
-    args = parse_args()
+    cli = parse_args()
+    args = cli.config["slurm"]
+    ngpus = int(args["slurm_additional_parameters"]["gres"][-1])
+    exp_dir = Path(f'./expts/{str(uuid.uuid4())}')
 
-    args.exp_dir.mkdir(parents=True, exist_ok=True)
-    executor = submitit.AutoExecutor(folder=args.exp_dir)
+    exp_dir.mkdir(parents=True, exist_ok=True)
+    executor = submitit.AutoExecutor(folder=exp_dir)
 
     executor.update_parameters(
-        name=args.job_name,
-        mem_gb=40 * args.ngpus,
-        gpus_per_node=args.ngpus,
-        tasks_per_node=args.ngpus,
-        cpus_per_task=10,
-        nodes=args.nodes,
-        timeout_min=args.timeout * 60,
-        slurm_partition=args.partition,
+        name=args["job_name"],
+        # gpus_per_node=args["ngpus"],
+        mem_gb=12 * ngpus,
+        tasks_per_node=ngpus,
+        cpus_per_task=8,
+        nodes=args["nodes"],
+        timeout_min=args["timeout"] * 60,
+        slurm_partition=args["partition"],
         slurm_signal_delay_s=120,
-        slurm_constraint=args.constraint,
-        slurm_comment=args.comment,
+        slurm_constraint=args["constraint"],
+        slurm_comment=args["comment"],
+        slurm_additional_parameters=args["slurm_additional_parameters"]
     )
 
-
-    job = executor.submit(main_vicreg.main, args)
+    job = executor.submit(protoclr_obow.slurm_main, cli)
     print("Submitted job_id:", job.job_id)
 
 
