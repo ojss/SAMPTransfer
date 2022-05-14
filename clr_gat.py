31a23b9a3c499dead458e6214f9f29a6d928f7a8
Ojas Shirekar <ojas.shirekar@ojas.dev>
2022-05-14 15:24:06 +0530

    updated finetuning methods
    added augmentations into ProtoTune along with sup con loss

--- clr_gat.py
+++ clr_gat.py
@@ -5,6 +5,7 @@ import uuid
 from typing import Optional, Iterable, Union, Tuple
 
 import einops
+import kornia as K
 import numpy as np
 import pl_bolts.optimizers
 import pytorch_lightning as pl
@@ -27,7 +28,24 @@ from proto_utils import (get_prototypes,
 from utils.sup_finetuning import Classifier
 
 
-# Cell
+class GNN(nn.Module):
+    def __init__(self, backbone: nn.Module, emb_dim: int, mpnn_dev: str, mpnn_opts: dict):
+        super(GNN, self).__init__()
+        self.backbone = backbone
+        self.emb_dim = emb_dim
+        self.mpnn_opts = mpnn_opts
+        mpnn_dev = mpnn_dev
+        self.gnn = GNNReID(mpnn_dev, mpnn_opts["gnn_params"], emb_dim)
+        self.graph_generator = GraphGenerator(mpnn_dev, **mpnn_opts["graph_params"])
+
+    def forward(self, x):
+        z = self.backbone(x).flatten(1)
+        z_cnn = z.clone()
+        edge_attr, edge_index, z = self.graph_generator.get_graph(z)
+        _, (z,) = self.gnn(z, edge_index, edge_attr, self.mpnn_opts["output_train_gnn"])
+        return z_cnn, z
+
+
 class CLRGAT(pl.LightningModule):
     def __init__(self,
                  n_support,
@@ -84,13 +102,13 @@ class CLRGAT(pl.LightningModule):
         self.sup_finetune_epochs = sup_finetune_epochs
         self.ft_freeze_backbone = ft_freeze_backbone
         self.finetune_batch_norm = finetune_batch_norm
+        self.img_orig_size = img_orig_size
 
         self.mpnn_opts = mpnn_opts
         if mpnn_opts["_use"]:
             _, in_dim = self.backbone(torch.randn(self.batch_size, 3, *img_orig_size)).flatten(1).shape
             self.dim = in_dim
-            self.gnn = GNNReID(mpnn_dev, mpnn_opts["gnn_params"], in_dim).to(self.device)
-            self.graph_generator = GraphGenerator(mpnn_dev, **mpnn_opts["graph_params"])
+            self.model = GNN(self.backbone, in_dim, mpnn_dev, mpnn_opts)
             self.mpnn_temperature = mpnn_opts["temperature"]
             if isinstance(mpnn_loss_fn, nn.Module):
                 self.gnn_loss = mpnn_loss_fn
@@ -101,8 +119,8 @@ class CLRGAT(pl.LightningModule):
 
     def mpnn_forward_pass(self, x_support, x_query, y_support, y_query, ways):
         loss_cnn = 0.
-        _, z_orig, (z,) = self.forward(torch.cat([x_support, x_query]),
-                                       torch.cat([y_support, y_query], 1).squeeze())
+        z_orig, z = self.mpnn_forward(torch.cat([x_support, x_query]),
+                                      torch.cat([y_support, y_query], 1).squeeze())
         if self.mpnn_opts["loss_cnn"]:
             loss_cnn, _ = self.calculate_protoclr_loss(z_orig, y_support, y_query, ways,
                                                        temperature=self.mpnn_temperature)
@@ -150,20 +168,20 @@ class CLRGAT(pl.LightningModule):
             ret['lr_scheduler'] = {'scheduler': sch, 'interval': 'step'}
         return ret
 
-    def forward(self, x, y=None) -> Tuple[torch.Tensor, torch.Tensor, Iterable]:
+    def mpnn_forward(self, x, y=None) -> Tuple[torch.Tensor, torch.Tensor]:
         """
 
         :param x: torch.Tensor
         :param y: torch.Tensor
-        :return: Tuple(preds, z_cnn, z)
+        :return: Tuple(z_cnn, z)
         """
-        z = self.backbone(x)
-        z_orig = z.clone().flatten(1)
-        z = z.flatten(1)
-        edge_attr, edge_index, z = self.graph_generator.get_graph(z, y)
-        preds, z = self.gnn(z, edge_index, edge_attr, self.mpnn_opts["output_train_gnn"])
+        z_cnn, z = self.model(x)
 
-        return preds, z_orig, z
+        return z_cnn, z
+
+    def forward(self, x):
+        _, z = self.model(x)
+        return z
 
     def calculate_protoclr_loss(self, z, y_support, y_query, ways, loss_fn=F.cross_entropy, temperature=1.):
 
@@ -252,34 +270,33 @@ class CLRGAT(pl.LightningModule):
         self.eval()
         proto = None
         if self.mpnn_opts["adapt"] == "task":
-            z_support = self.backbone(x_a_i).flatten(1)
-            z_query = self.backbone(x_b_i).flatten(1)
+            z_support = self.model.backbone(x_a_i).flatten(1)
+            z_query = self.model.backbone(x_b_i).flatten(1)
             nmb_proto = n_way
             z_proto = z_support.view(nmb_proto, n_support, -1).mean(1)
             combined = torch.cat([z_proto, z_query])
             edge_attr, edge_index, combined = self.graph_generator.get_graph(combined, Y=None)
-            _, (combined,) = self.gnn(combined, edge_index, edge_attr, self.mpnn_opts["output_train_gnn"])
+            _, (combined,) = self.model.gnn(combined, edge_index, edge_attr, self.mpnn_opts["output_train_gnn"])
             proto, query = combined.split([nmb_proto, len(z_query)])  # split based on number of prototypes
             z_a_i = z_support
         elif self.mpnn_opts["adapt"] == "proto_only":
             # instance level feature sharing
             combined = torch.cat([x_a_i, x_b_i])
-            combined = self.backbone(combined).flatten(1)
+            combined = self.model.backbone(combined).flatten(1)
             z_support, z_query = combined.split([n_support * n_way, len(x_b_i)])
             z_proto = z_support.view(n_way, n_support, -1).mean(1)
             edge_attr, edge_index, z_proto = self.graph_generator.get_graph(z_proto, Y=None)
-            _, (z_proto,) = self.gnn(z_proto, edge_index, edge_attr, self.mpnn_opts["output_train_gnn"])
+            _, (z_proto,) = self.model.gnn(z_proto, edge_index, edge_attr, self.mpnn_opts["output_train_gnn"])
             proto = z_proto
             z_a_i = z_support
         elif self.mpnn_opts["adapt"] == "instance":
             # TODO: change instance to include both x_a_i and x_b_i
             combined = torch.cat([x_a_i, x_b_i])
-            _, _, (combined,) = self.forward(combined)
+            _, combined = self.mpnn_forward(combined)
             z_a_i, _ = combined.split([len(x_a_i), len(x_b_i)])
         else:
-            z_a_i = self.backbone(x_a_i).flatten(1)
+            z_a_i = self.model.backbone(x_a_i).flatten(1)
         self.train()
-
         # Define linear classifier
         input_dim = z_a_i.shape[1]
         classifier = Classifier(input_dim, n_way=n_way)
@@ -287,17 +304,23 @@ class CLRGAT(pl.LightningModule):
         classifier.train()
         ###############################################################################################
         loss_fn = nn.CrossEntropyLoss().to(device)
+        sup_con_loss = losses.SupConLoss()
+        augs = nn.Sequential(K.augmentation.ColorJitter(brightness=.4, contrast=.4, saturation=.4, hue=.1, p=0.8),
+                             K.augmentation.RandomResizedCrop(size=self.img_orig_size, scale=(0.5, 1.)),
+                             K.augmentation.RandomHorizontalFlip(),
+                             K.augmentation.RandomGrayscale(p=.2),
+                             K.augmentation.RandomGaussianBlur(kernel_size=(3, 3),
+                                                               sigma=(0.1, 2.0)))
         # Initialise as distance classifer (distance to prototypes)
         if proto_init:
             classifier.init_params_from_prototypes(z_a_i, n_way, n_support, z_proto=proto)
         classifier_opt = torch.optim.Adam(classifier.parameters(), lr=inner_lr)
         if freeze_backbone is False:
             delta_opt = torch.optim.Adam(
-                filter(lambda p: p.requires_grad, self.parameters()), lr=inner_lr)
-            # TODO: add provisions for EdgeConv layer here until then freeze_backbone=False
+                filter(lambda p: p.requires_grad, self.model.gnn.parameters()), lr=inner_lr)
         # Finetuning
         if freeze_backbone is False:
-            self.train()
+            self.model.gnn.train()
         else:
             self.eval()
         classifier.train()
@@ -320,16 +343,18 @@ class CLRGAT(pl.LightningModule):
                     rand_id[j: min(j + batch_size, support_size)]).to(device)
 
                 z_batch = x_a_i[selected_id]
-                y_batch = y_a_i[selected_id]
+                z_batch = torch.cat([z_batch, augs(z_batch)])
+                y_batch = y_a_i[selected_id].repeat(2)
                 #####################################
                 # TODO: should only instance adaptation be used below?
                 if self.mpnn_opts["adapt"] in ["task", "proto_only", "instance"]:
-                    _, _, (output,) = self.forward(z_batch, y_batch)
+                    _, output = self.mpnn_forward(z_batch, y_batch)
                 else:
-                    output = self.backbone(z_batch).flatten(1)
-
-                output = classifier(output)
-                loss = loss_fn(output, y_batch)
+                    output = self.model.backbone(z_batch).flatten(1)
+                preds = classifier(output)
+                loss = loss_fn(preds, y_batch)
+                if freeze_backbone is False:
+                    loss += sup_con_loss(output, y_batch)
 
                 #####################################
                 loss.backward()
@@ -355,11 +380,10 @@ class CLRGAT(pl.LightningModule):
         # cannot do proto adapt here
         elif self.mpnn_opts["adapt"] == "instance":
             combined = torch.cat([x_a_i, x_b_i])
-            _, _, (combined,) = self.forward(combined)
+            _, combined = self.mpnn_forward(combined)
             _, output = combined.split([len(x_a_i), len(x_b_i)])
         else:
             output = self.backbone(x_b_i).flatten(1)
-        breakpoint()
         scores = classifier(output)
 
         loss = F.cross_entropy(scores, y_query, reduction='mean')
@@ -382,45 +406,49 @@ class CLRGAT(pl.LightningModule):
         batch_size = self.eval_ways
         support_size = self.eval_ways * n_support
         y_supp = Variable(torch.from_numpy(np.repeat(range(self.eval_ways), n_support))).to(self.device)
+        y_query = torch.tensor(np.repeat(range(self.eval_ways), n_query)).to(self.device)
+
         self.eval()
-        _, _, (z_supp,) = self.forward(x_support_var)
-        # z_proto = get_prototypes(z_supp, y_supp, self.eval_ways)
+        _, z = self.mpnn_forward(torch.cat([x_support_var, x_query_var]))
+        z_supp, _ = z.split([len(x_support), len(x_query)])
         classifier = Classifier(z_supp.shape[-1], self.eval_ways)
         classifier.init_params_from_prototypes(z_support=z_supp, n_way=self.eval_ways, n_support=n_support)
         classifier.to(self.device)
-        self.train()
-        classifier.train()
         ce_loss = nn.CrossEntropyLoss().to(self.device)
         sup_con_loss = losses.SupConLoss()
+        local_model = copy.deepcopy(self.model)
+        local_model.train()
+        classifier.train()
         if not self.ft_freeze_backbone:
             # Only freeze the CNN backbone
-            self.backbone.requires_grad_(False)
-            self.gnn.requires_grad_(True)
-            # TODO: should I use another projector layer here instead of touching the GAT?
-        parameters = list(filter(lambda p: p.requires_grad, self.gnn.parameters())) + list(classifier.parameters())
-        opt = torch.optim.Adam(parameters, lr=self.lr, weight_decay=self.weight_decay)
+            local_model.backbone.requires_grad_(False)
+            local_model.gnn.requires_grad_(True)
+        # TODO: should I use another projector layer here instead of touching the GAT?
+        backbone_parameters = list(filter(lambda p: p.requires_grad, local_model.parameters()))
+        classifier_params = list(classifier.parameters())
+        delta_opt = torch.optim.Adam(backbone_parameters, lr=self.lr, weight_decay=self.weight_decay)
+        classifier_opt = torch.optim.Adam(classifier_params, lr=self.sup_finetune_lr, weight_decay=self.weight_decay)
+
         for _ in range(self.sup_finetune_epochs):
-            opt.zero_grad()
-            _, _, (outputs,) = self.forward(x_support_var)
+            # MAML inner loop
+            delta_opt.zero_grad()
+            classifier_opt.zero_grad()
+            _, outputs = local_model(torch.cat([x_support_var, x_query_var]))
+            outputs, _ = outputs.split([len(x_support), len(x_query)])
             preds = classifier(outputs)
             loss1 = ce_loss(preds, y_supp)
             loss2 = sup_con_loss(outputs, y_supp)
             loss = loss1 + loss2
             loss.backward()
-            opt.step()
-        breakpoint()
-        self.eval()
-        classifier.eval()
-        y_query = torch.tensor(np.repeat(range(self.eval_ways), n_query)).to(self.device)
-        combined = torch.cat([x_support_var, x_query_var])
-        # combined = x_query_var
-        _, _, (z,) = self.forward(combined)
-        _, z_query = z.split([len(x_support), len(x_query)])
-        # z_query = z
-        scores = classifier(z_query)
-        loss = F.cross_entropy(scores, y_query, reduction="mean")
-        _, preds = torch.max(scores, dim=1)
-        acc = accuracy(preds, y_query)
+            delta_opt.step()
+            classifier_opt.step()
+        _, outputs = local_model(x_query_var)
+        scores = classifier(outputs)
+        loss = F.cross_entropy(scores, y_query, reduction="mean") + sup_con_loss(outputs, y_query, )
+        _, predictions = torch.max(scores, dim=1)
+        # run local_model on query points
+        acc = accuracy(predictions, y_query)
+
         return loss, acc
 
     def std_proto_form(self, batch, batch_idx):
@@ -445,7 +473,7 @@ class CLRGAT(pl.LightningModule):
             z = self.backbone(x)
             z = einops.rearrange(z, "b c h w -> 1 b (c h w)")
         else:
-            _, _, (z,) = self.forward(x)
+            _, z = self.mpnn_forward(x)
             z = einops.rearrange(z, "b e -> 1 b e")
         z_support = z[:, :self.eval_ways * shots]
         z_query = z[:, self.eval_ways * shots:]
