999e1eede57c1897599a7fa24576e71c78a8b5d4
Ojas Shirekar <ojas.shirekar@ojas.dev>
2022-06-05 19:26:45 +0200

    comitting long completed byol

---
+++ byol.py
@@ -0,0 +1,99 @@
+import torch
+from torch import nn
+import torchvision
+import copy
+import pytorch_lightning as pl
+
+from lightly.data import LightlyDataset
+from lightly.data import SimCLRCollateFunction
+from lightly.loss import NegativeCosineSimilarity
+from lightly.models.modules import BYOLProjectionHead
+from lightly.models.utils import deactivate_requires_grad
+from lightly.models.utils import update_momentum
+from torch.utils.data import DataLoader
+
+
+class BYOL(pl.LightningModule):
+    def __init__(self, arch: str, data_path: str, input_size: int, batch_size: int, num_workers: int):
+        super().__init__()
+        if arch in torchvision.models.__dict__.keys():
+            resnet = torchvision.models.__dict__[arch](pretrained=False)
+            self.backbone = nn.Sequential(*list(resnet.children())[:-1])
+        self.projection_head = BYOLProjectionHead(512, 1024, 256)
+        self.prediction_head = BYOLProjectionHead(256, 1024, 256)
+
+        self.backbone_momentum = copy.deepcopy(self.backbone)
+        self.projection_head_momentum = copy.deepcopy(self.projection_head)
+
+        deactivate_requires_grad(self.backbone_momentum)
+        deactivate_requires_grad(self.projection_head_momentum)
+
+        self.criterion = NegativeCosineSimilarity()
+
+        self.data_path = data_path
+        self.input_size = input_size
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        self.save_hyperparameters()
+
+    def forward(self, x):
+        y = self.backbone(x).flatten(start_dim=1)
+        z = self.projection_head(y)
+        p = self.prediction_head(z)
+        return p
+
+    def forward_momentum(self, x):
+        y = self.backbone_momentum(x).flatten(start_dim=1)
+        z = self.projection_head_momentum(y)
+        z = z.detach()
+        return z
+
+    def training_step(self, batch, batch_idx):
+        update_momentum(self.backbone, self.backbone_momentum, m=0.99)
+        update_momentum(self.projection_head, self.projection_head_momentum, m=0.99)
+        (x0, x1), _, _ = batch
+        p0 = self.forward(x0)
+        z0 = self.forward_momentum(x0)
+        p1 = self.forward(x1)
+        z1 = self.forward_momentum(x1)
+        loss = 0.5 * (self.criterion(p0, z1) + self.criterion(p1, z0))
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.parameters(), lr=0.06)
+
+    def train_dataloader(self):
+        miniimg = torchvision.datasets.ImageFolder(self.data_path)
+        dataset = LightlyDataset.from_torch_dataset(miniimg)
+
+        collate_fn = SimCLRCollateFunction(input_size=self.input_size)
+
+        dataloader = DataLoader(dataset, batch_size=self.batch_size, collate_fn=collate_fn, shuffle=True,
+                                drop_last=True,
+                                num_workers=self.num_workers)
+        return dataloader
+
+
+model = BYOL()
+
+cifar10 = torchvision.datasets.CIFAR10("datasets/cifar10", download=True)
+dataset = LightlyDataset.from_torch_dataset(cifar10)
+# or create a dataset from a folder containing images or videos:
+# dataset = LightlyDataset("path/to/folder")
+
+collate_fn = SimCLRCollateFunction(input_size=32)
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=256,
+    collate_fn=collate_fn,
+    shuffle=True,
+    drop_last=True,
+    num_workers=8,
+)
+
+gpus = 1 if torch.cuda.is_available() else 0
+
+trainer = pl.Trainer(max_epochs=10, gpus=gpus)
+trainer.fit(model=model, train_dataloaders=dataloader)
