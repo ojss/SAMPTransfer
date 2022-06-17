7e138051e55e6c93e527f4f514f46ae357d3bc1e
Ojas Shirekar <ojas.shirekar@ojas.dev>
2022-06-17 20:32:15 +0200

    added slurm job and updated conf

--- cdfsl-benchmark/configs.py
+++ cdfsl-benchmark/configs.py
@@ -1,9 +1,18 @@
-save_dir                    = './logs'
+# save_dir                    = './logs'
+#
+# # miniImageNet_path           	= '../../semifew_data/miniImagenetFullSize'
+# miniImageNet_path = '/home/ojas/projects/unsupervised-meta-learning/data/untarred/miniImagenetFullSize'
+#
+# ISIC_path = "../../semifew_data/ISIC"
+# ChestX_path = "../../semifew_data/chestX"
+# CropDisease_path = "../../semifew_data/plant-disease"
+# EuroSAT_path = "/home/ojas/projects/unsupervised-meta-learning/data/untarred/EuroSat"
 
-# miniImageNet_path           	= '../../semifew_data/miniImagenetFullSize'
-miniImageNet_path = '/home/ojas/projects/unsupervised-meta-learning/data/untarred/miniImagenetFullSize'
+save_dir = './logs'
 
-ISIC_path = "../../semifew_data/ISIC"
-ChestX_path = "../../semifew_data/chestX"
-CropDisease_path = "../../semifew_data/plant-disease"
-EuroSAT_path = "/home/ojas/projects/unsupervised-meta-learning/data/untarred/EuroSat"
+miniImageNet_path = '/home/nfs/oshirekar/unsupervised_ml/data/cdfsl/miniImagenetFullSize/'
+
+ISIC_path = "/home/nfs/oshirekar/unsupervised_ml/data/cdfsl/ISIC"
+ChestX_path = "/home/nfs/oshirekar/unsupervised_ml/data/cdfsl/chestx"
+CropDisease_path = "/home/nfs/oshirekar/unsupervised_ml/data/cdfsl/plant-disease"
+EuroSAT_path = "/home/nfs/oshirekar/unsupervised_ml/data/cdfsl/2750"
